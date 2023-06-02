c9e75cd842bf9793bd026daeb6ffb0f6f7dda4e6
z4nder <gusttavo212@gmail.com>
2023-06-01 21:08:06 -0300

    First commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import Greet from "./components/Greet.vue";
+</script>
+
+<template>
+  <div class="container">
+    <h1>Hive Pomodoro</h1>
+
+    <div class="row">
+      <a href="https://vitejs.dev" target="_blank">
+        <img src="./assets/logo_render.svg" class="logo vite" alt="Vite logo" />
+      </a>
+    </div>
+
+    <Greet />
+  </div>
+</template>
+
+<style scoped>
+.logo.vite:hover {
+  filter: drop-shadow(0 0 2em #747bff);
+}
+
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #249b73);
+}
+</style>
