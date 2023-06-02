93ccd66c1f9b2922fdb8931d253f425cbf4e3885
z4nder <gusttavo212@gmail.com>
2023-06-01 21:41:39 -0300

    Add example

--- src/components/Pomodoro.vue
+++ src/components/Pomodoro.vue
@@ -25,5 +25,10 @@ async function greet() {
     </div>
   </form>
 
-  <p>{{ greetMsg }}</p>
+  <!-- <form class="row" @submit.prevent="greet">
+    <input id="greet-input" v-model="name" placeholder="Enter a name..." />
+    <button type="submit">Greet</button>
+  </form>
+
+  <p>{{ greetMsg }}</p> -->
 </template>
