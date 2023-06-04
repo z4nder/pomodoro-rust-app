0cb46124e3e68b17fc192327ad31b22f498e39aa
z4nder <gusttavo212@gmail.com>
2023-06-04 20:52:17 -0300

    Change buttons name

--- src/components/Pomodoro.vue
+++ src/components/Pomodoro.vue
@@ -57,7 +57,13 @@ function formatSecondsValue(seconds: number): String {
         @click="stop"
         class="mt-5 w-full rounded-md bg-primary-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-primary-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-600"
       >
-        Stop
+        Pause
+      </button>
+      <button
+        @click="stop"
+        class="mt-5 w-full rounded-md bg-primary-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-primary-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-600"
+      >
+        Reset
       </button>
     </div>
   </div>
